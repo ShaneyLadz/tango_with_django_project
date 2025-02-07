3984126af771cca00d3ab2049a0259835d428cec
ShaneyLadz <itzshaneladz@gmail.com>
2025-02-07 15:51:51 +0000

    CH5-Exercises completed

--- rango/models.py
+++ rango/models.py
@@ -8,6 +8,8 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
         
     name = models.CharField(max_length = 128, unique = True)
+    views = models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
 
     def __str__(self):
         return self.name
